29d9af632b755db281c26ef85db148489d6b5fc4
Aissata-Sow788 <saissata676@gmail.com>
2026-04-11 20:10:43 +0000

    configuration base de donnee mysql

--- config/settings.py
+++ config/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'joj_news',
+        'USER': 'root',
+        'PASSWORD': 'MotDePasseFort',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
